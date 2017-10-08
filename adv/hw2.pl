153ca478b44e487812c3e2c25125a54a8918bd75
Joseph Sundberg <joseph.sundberg@gmail.com>
2017-10-07 22:25:07 -0600

    finshied

--- adv/hw2.pl
+++ adv/hw2.pl
@@ -1,13 +1,33 @@
 :-include(week1).
 
+%checked look
+look_c(P):-here(P),look.
+
+list_things_i:-has(X),tab(2),write_name(X),write(" -"),tab(1),write_short(X),nl,!.
+
+%inventory
+inventory:-list_things_i.
+
+%checked studdy
+study_c(Place):-here(Place),study(Place).
+study_c(Thing):-location(Thing,P),here(P),study(Thing).
+study_c(Container):-location(Container,P),here(P),study(Container).
+
 %unchecked move
-move(P):-room(P),retract(here(P)),asserta(here(P)),look(P).
+move(P):-here(X),room(P),retract(here(X)),asserta(here(P)),look(P).
 
 %checked move
-move_c(P):-here(X),room(P),connection(X,P),!,retract(here(P)),asserta(here(P)),look(P).
+move_c(P):-here(X),room(P),connection(X,P),retract(here(X)),asserta(here(P)),look(P).
 
 %unchecked Take
-take(I):-asserta(has(I)),location(I,L),retract(location(I,L)),asserta(location(I,inventory)).
+take(I):-is_heavy(I),asserta(has(I)),location(I,L),retract(location(I,L)),asserta(location(I,inven)).
+is_heavy(I):-not(heavy(I)),!.
+
+%checked take
+take_c(I):-is_heavy(I),here(X),location(I,X),asserta(has(I)),location(I,L),retract(location(I,L)),asserta(location(I,inven)).
 
 %unchecked Put
-put(I):-has(I),here(P),location(I,X),retract(has(I)),asserta(location(I,P)).
+put(I,P):-has(I),retract(has(I)),asserta(location(I,P)).
+
+%chected put
+put(I)_c:-has(I),here(P),retract(has(I)),asserta(location(I,P)).
